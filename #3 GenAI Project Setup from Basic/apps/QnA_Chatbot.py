318f13c83c2f56305995e27cb81c419bd8fdfa15
Nikunj Shetye <nikunj.shetye@gmail.com>
2026-07-18 17:17:37 -0400

    files changes some bug fixes

--- #3 GenAI Project Setup from Basic/apps/QnA_Chatbot.py
+++ #3 GenAI Project Setup from Basic/apps/QnA_Chatbot.py
@@ -4,6 +4,7 @@ load_dotenv()
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import InMemorySaver
 
 
 search = GoogleSerperAPIWrapper()
@@ -12,7 +13,8 @@ model =ChatOpenAI(model="gpt-4o")
 agent = create_agent(
     model=model,
     tools=[search.run],
-    system_prompt="you are a agent who search google for answer based on the question."
+    system_prompt="you are a agent who search google for answer based on the question.",
+    checkpointer=InMemorySaver()
 )
 
 
@@ -20,5 +22,6 @@ while True:
     query = input("User : ")
     if query.lower() == "exit":
         break
-    response = agent.invoke({"messages":[{"role":"user","content":query}]})
+    response = agent.invoke({"messages":[{"role":"user","content":query}]},
+                            {"configurable": {"thread_id" :"1"}},)
     print("ai_system: ",response["messages"][-1].content)
